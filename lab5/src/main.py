221c9ca9a47bc6fb7efb4e13149cc4ef42b18cd6
LLK <arturprokopenko01@gmail.com>
2022-11-30 20:02:25 +0300

    db works!

--- lab5/src/main.py
+++ lab5/src/main.py
@@ -1,13 +1,8 @@
 from core.service_layer.unit_of_work.sqlalchemy_unit_of_work import SqlAlchemyUnitOfWork
 from core.domain.bus import Bus
+from core.domain.train import Train
 from core.adapters import orm
-
+from sqlalchemy import create_engine
 
 orm.start_mappers()
 uow = SqlAlchemyUnitOfWork()
-with uow:
-    uow.buses.add(Bus(fio='Driver\'s FIO', bus_number="LAWYERUP", route_number=1,
-                      brand="LLK", creation_year=2010, vehicle=2.3))
-    buses = uow.buses.list()
-    for item in buses:
-        print(item)
